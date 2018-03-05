74d89721e657180e2dd03dc87391939e8e068cb4
ger0g <37080025+ger0g@users.noreply.github.com>
2018-03-05 12:07:50 -0500

    Add files via upload

---
+++ Engine/World.cs
@@ -0,0 +1,210 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Engine
+{
+    public static class World
+    {
+        private static readonly List<Item> _items = new List<Item>();
+        private static readonly List<Monster> _monsters = new List<Monster>();
+        private static readonly List<Quest> _quests = new List<Quest>();
+        private static readonly List<Location> _locations = new List<Location>();
+
+        public const int UNSELLABLE_ITEM_PRICE = -1;
+
+        public const int ITEM_ID_RUSTY_SWORD = 1;
+        public const int ITEM_ID_RAT_TAIL = 2;
+        public const int ITEM_ID_PIECE_OF_FUR = 3;
+        public const int ITEM_ID_SNAKE_FANG = 4;
+        public const int ITEM_ID_SNAKESKIN = 5;
+        public const int ITEM_ID_CLUB = 6;
+        public const int ITEM_ID_HEALING_POTION = 7;
+        public const int ITEM_ID_SPIDER_FANG = 8;
+        public const int ITEM_ID_SPIDER_SILK = 9;
+        public const int ITEM_ID_ADVENTURER_PASS = 10;
+        public const int ITEM_ID_STRONG_SILK = 11;
+        public const int ITEM_ID_CYBER_EYE = 12;
+
+        public const int MONSTER_ID_RAT = 1;
+        public const int MONSTER_ID_SNAKE = 2;
+        public const int MONSTER_ID_AUGMENTED_SPIDER = 3;
+
+        public const int QUEST_ID_CLEAR_ALCHEMIST_GARDEN = 1;
+        public const int QUEST_ID_CLEAR_FARMERS_FIELD = 2;
+
+        public const int LOCATION_ID_HOME = 1;
+        public const int LOCATION_ID_TOWN_SQUARE = 2;
+        public const int LOCATION_ID_GUARD_POST = 3;
+        public const int LOCATION_ID_ALCHEMIST_HUT = 4;
+        public const int LOCATION_ID_ALCHEMISTS_GARDEN = 5;
+        public const int LOCATION_ID_FARMHOUSE = 6;
+        public const int LOCATION_ID_FARM_FIELD = 7;
+        public const int LOCATION_ID_BRIDGE = 8;
+        public const int LOCATION_ID_STRANGE_BUILDING = 9;
+
+        static World()
+        {
+            PopulateItems();
+            PopulateMonsters();
+            PopulateQuests();
+            PopulateLocations();
+        }
+
+        private static void PopulateItems()
+        {
+
+            //weapons (1, 2, 3) 1= min dam 2= max dam 3 = value
+            //items (#) = value
+            _items.Add(new Weapon(ITEM_ID_RUSTY_SWORD, "Rusty sword", "Rusty swords", 0, 5, 5));
+            _items.Add(new Item(ITEM_ID_RAT_TAIL, "Rat tail", "Rat tails", 1));
+            _items.Add(new Item(ITEM_ID_PIECE_OF_FUR, "Piece of fur", "Pieces of fur", 1));
+            _items.Add(new Item(ITEM_ID_SNAKE_FANG, "Snake fang", "Snake fangs", 1));
+            _items.Add(new Item(ITEM_ID_SNAKESKIN, "Snakeskin", "Snakeskins", 2));
+            _items.Add(new Weapon(ITEM_ID_CLUB, "Club", "Clubs", 3, 10, 8));
+            _items.Add(new HealingPotion(ITEM_ID_HEALING_POTION, "Healing potion", "Healing potions", 5, 3));
+            _items.Add(new Item(ITEM_ID_SPIDER_FANG, "Spider fang", "Spider fangs", 1));
+            _items.Add(new Item(ITEM_ID_SPIDER_SILK, "Spider silk", "Spider silks", 1));
+            _items.Add(new Item(ITEM_ID_ADVENTURER_PASS, "Adventurer pass", "Adventurer passes", UNSELLABLE_ITEM_PRICE));
+        }
+
+        private static void PopulateMonsters()
+        {
+            //monster "name", max damage, exp, gold, current Hitpoints, max hitpoints
+            //loot item (ITEM_ID_SNAKESKIN), 75, true)) 75= chance to drop
+            //Low Monsters
+            Monster rat = new Monster(MONSTER_ID_RAT, "Rat", 5, 3, 10, 3, 3);
+            rat.LootTable.Add(new LootItem(ItemByID(ITEM_ID_RAT_TAIL), 75, false));
+            rat.LootTable.Add(new LootItem(ItemByID(ITEM_ID_PIECE_OF_FUR), 75, true));
+
+            Monster snake = new Monster(MONSTER_ID_SNAKE, "Snake", 5, 3, 10, 3, 3);
+            snake.LootTable.Add(new LootItem(ItemByID(ITEM_ID_SNAKE_FANG), 75, false));
+            snake.LootTable.Add(new LootItem(ItemByID(ITEM_ID_SNAKESKIN), 75, true));
+            //Med Monsters
+
+            //Boss
+            Monster AugmentedSpider = new Monster(MONSTER_ID_AUGMENTED_SPIDER, "Augmented spider", 20, 5, 40, 10, 10);
+            AugmentedSpider.LootTable.Add(new LootItem(ItemByID(ITEM_ID_SPIDER_FANG), 75, true));
+            AugmentedSpider.LootTable.Add(new LootItem(ItemByID(ITEM_ID_STRONG_SILK), 25, false));
+
+            _monsters.Add(rat);
+            _monsters.Add(snake);
+            _monsters.Add(AugmentedSpider);
+        }
+
+        private static void PopulateQuests()
+        {
+            Quest clearAlchemistGarden =
+                new Quest(
+                    QUEST_ID_CLEAR_ALCHEMIST_GARDEN,
+                    "Clear the alchemist's garden",
+                    "Kill rats in the alchemist's garden and bring back 3 rat tails. You will receive a healing potion and 10 gold pieces.", 20, 10);
+
+            clearAlchemistGarden.QuestCompletionItems.Add(new QuestCompletionItem(ItemByID(ITEM_ID_RAT_TAIL), 3));
+
+            clearAlchemistGarden.RewardItem = ItemByID(ITEM_ID_HEALING_POTION);
+
+            Quest clearFarmersField =
+                new Quest(
+                    QUEST_ID_CLEAR_FARMERS_FIELD,
+                    "Clear the farmer's field",
+                    "Kill snakes in the farmer's field and bring back 3 snake fangs. You will receive an adventurer's pass and 20 gold pieces.", 20, 20);
+
+            clearFarmersField.QuestCompletionItems.Add(new QuestCompletionItem(ItemByID(ITEM_ID_SNAKE_FANG), 3));
+
+            clearFarmersField.RewardItem = ItemByID(ITEM_ID_ADVENTURER_PASS);
+
+            _quests.Add(clearAlchemistGarden);
+            _quests.Add(clearFarmersField);
+        }
+
+        private static void PopulateLocations()
+        {
+            // Create each location
+            Location home = new Location(LOCATION_ID_HOME, "Facility", "A Cryo Facility. At least that is what it says on the wall in front of you as you stand up on the slippery wet floor. behind you is the broken pod where you used to be. You deside to go outside");
+
+            Location townSquare = new Location(LOCATION_ID_TOWN_SQUARE, "Town square", "You see a small collection of huts and makeshift houses all surrounding a small, run down fountain. Off to the side you see a man in a long tunic trying to sell different things made of presumably rat fur, due to the small size and shape of the pelts attached to his belt.");
+
+            Vendor bobTheRatCatcher = new Vendor("Rat-Catcher");
+            bobTheRatCatcher.AddItemToInventory(ItemByID(ITEM_ID_PIECE_OF_FUR), 5);
+            bobTheRatCatcher.AddItemToInventory(ItemByID(ITEM_ID_RAT_TAIL), 3);
+
+            townSquare.VendorWorkingHere = bobTheRatCatcher;
+
+            Location alchemistHut = new Location(LOCATION_ID_ALCHEMIST_HUT, "Alchemist's hut", "You see a medium sized wooden hut that looks a bit more comfortable than the other huts in town. Through the open door you see that there are many strange plants on the shelves.");
+            alchemistHut.QuestAvailableHere = QuestByID(QUEST_ID_CLEAR_ALCHEMIST_GARDEN);
+
+            Location alchemistsGarden = new Location(LOCATION_ID_ALCHEMISTS_GARDEN, "Alchemist's garden", "Past the Alchemist's hut you see a large field with many plants growing there.");
+            alchemistsGarden.MonsterLivingHere = MonsterByID(MONSTER_ID_RAT);
+
+            Location farmhouse = new Location(LOCATION_ID_FARMHOUSE, "Farmhouse", "There is a small farmhouse, shakily constructed, with a farmer in front looking visably shaken and distressed.");
+            farmhouse.QuestAvailableHere = QuestByID(QUEST_ID_CLEAR_FARMERS_FIELD);
+
+            Location farmersField = new Location(LOCATION_ID_FARM_FIELD, "Farmer's field", "Past the farm house you see a small field with rows of vegetables.");
+            farmersField.MonsterLivingHere = MonsterByID(MONSTER_ID_SNAKE);
+
+            Location guardPost = new Location(LOCATION_ID_GUARD_POST, "Guard post", "There is a large, tough-looking guard with a metalic sheen showing from under his loosely fitting robe. He blocks your path and tells you to hand him the Adventurer pass", ItemByID(ITEM_ID_ADVENTURER_PASS));
+
+            Location bridge = new Location(LOCATION_ID_BRIDGE, "Bridge", "A stone bridge that is falling apart. It crosses a wide river with angry and sickly looking fish jumping out of the water.");
+
+            Location strangeBuilding = new Location(LOCATION_ID_STRANGE_BUILDING, "Strange Building", "You see a strange building disturbingly white in its cleanliness, you want up to the doors as the slide open for you. As you step inside you see spider webs covering covering the walls before you hear a blood curtling screech.");
+            strangeBuilding.MonsterLivingHere = MonsterByID(MONSTER_ID_AUGMENTED_SPIDER);
+
+            // Link the locations together
+            home.LocationToNorth = townSquare;
+
+            townSquare.LocationToNorth = alchemistHut;
+            townSquare.LocationToSouth = home;
+            townSquare.LocationToEast = guardPost;
+            townSquare.LocationToWest = farmhouse;
+
+            farmhouse.LocationToEast = townSquare;
+            farmhouse.LocationToWest = farmersField;
+
+            farmersField.LocationToEast = farmhouse;
+
+            alchemistHut.LocationToSouth = townSquare;
+            alchemistHut.LocationToNorth = alchemistsGarden;
+
+            alchemistsGarden.LocationToSouth = alchemistHut;
+
+            guardPost.LocationToEast = bridge;
+            guardPost.LocationToWest = townSquare;
+
+            bridge.LocationToWest = guardPost;
+            bridge.LocationToEast = strangeBuilding;
+
+            strangeBuilding.LocationToWest = bridge;
+
+            // Add the locations to the static list
+            _locations.Add(home);
+            _locations.Add(townSquare);
+            _locations.Add(guardPost);
+            _locations.Add(alchemistHut);
+            _locations.Add(alchemistsGarden);
+            _locations.Add(farmhouse);
+            _locations.Add(farmersField);
+            _locations.Add(bridge);
+            _locations.Add(strangeBuilding);
+        }
+
+        public static Item ItemByID(int id)
+        {
+            return _items.SingleOrDefault(x => x.ID == id);
+        }
+
+        public static Monster MonsterByID(int id)
+        {
+            return _monsters.SingleOrDefault(x => x.ID == id);
+        }
+
+        public static Quest QuestByID(int id)
+        {
+            return _quests.SingleOrDefault(x => x.ID == id);
+        }
+
+        public static Location LocationByID(int id)
+        {
+            return _locations.SingleOrDefault(x => x.ID == id);
+        }
+    }
+}
---
+++ SimpleRPG/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using SimpleRPG;
+using Engine;
+
+namespace SimpleRPG
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new SuperAdventure.SuperAdventure());
+        }
+
+        
+    }
+}
